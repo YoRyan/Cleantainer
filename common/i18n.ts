f10e7d2e28beaa357d3a2b4963bc10b30daeb188
Ryan Young <ryan@youngryan.com>
2025-11-25 03:48:10 +0000

    chore: remove dead code

--- common/i18n.ts
+++ common/i18n.ts
@@ -13,14 +13,3 @@ class MessageElement extends HTMLSpanElement {
     }
 }
 customElements.define("i18n-message", MessageElement, { extends: "span" });
-
-async function i18nMain() {
-    const options = document.querySelectorAll(
-        "option[data-i18n-message]",
-    ) as NodeListOf<HTMLOptionElement>;
-    for (const option of options) {
-        const { text } = option;
-        option.text = browser.i18n.getMessage(text);
-    }
-}
-i18nMain();
