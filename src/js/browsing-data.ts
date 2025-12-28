b7ec3ae5a28b282c47088ed5b8cf62f13298a5fa
Ryan Young <ryan@youngryan.com>
2025-12-28 00:34:34 -0500

    docs: clarify limitations for the private browsing container

--- src/js/browsing-data.ts
+++ src/js/browsing-data.ts
@@ -1,4 +1,24 @@
 export async function cleanContainer(cookieStoreId: string) {
+    let dataToRemove: browser.browsingData.DataTypeSet;
+    if (cookieStoreId === "firefox-private") {
+        // As of Firefox 146, clearing IndexedDB and localStorage for the
+        // private container does not work and silently fails.
+        // https://github.com/mozilla-firefox/firefox/blob/main/toolkit/components/extensions/test/mochitest/test_ext_browsingData_indexedDB.html
+        // https://github.com/mozilla-firefox/firefox/blob/main/toolkit/components/extensions/test/mochitest/test_ext_browsingData_localStorage.html
+        dataToRemove = {
+            cookies: true,
+            // Clearing sessionStorage works. That's tied to the localStorage
+            // flag.
+            // https://github.com/mozilla-firefox/firefox/blob/main/toolkit/components/extensions/test/mochitest/test_ext_browsingData_sessionStorage.html
+            localStorage: true,
+        };
+    } else {
+        dataToRemove = {
+            cookies: true,
+            indexedDB: true,
+            localStorage: true,
+        };
+    }
     await browser.browsingData.remove(
         {
             cookieStoreId,
@@ -8,10 +28,6 @@ export async function cleanContainer(cookieStoreId: string) {
                 extension: false,
             },
         },
-        {
-            cookies: true,
-            indexedDB: true,
-            localStorage: true,
-        },
+        dataToRemove,
     );
 }
